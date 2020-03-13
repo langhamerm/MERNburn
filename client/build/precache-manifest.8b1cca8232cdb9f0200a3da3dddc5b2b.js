df99774463e37506fc44137788e4de10a4ae2c51
Mark Langhamer <langhamerm@gmail.com>
2020-03-13 13:54:49 -0500

    yo

---
+++ client/build/precache-manifest.8b1cca8232cdb9f0200a3da3dddc5b2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7930d6f3e6d94b6cc9746fbb6d1a5098",
+    "url": "/index.html"
+  },
+  {
+    "revision": "068ae92a92f8c5f91166",
+    "url": "/static/css/2.19d62180.chunk.css"
+  },
+  {
+    "revision": "068ae92a92f8c5f91166",
+    "url": "/static/js/2.374706b0.chunk.js"
+  },
+  {
+    "revision": "ea4b5ae141dc8c71e175204f6e8e684d",
+    "url": "/static/js/2.374706b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c2ff51320165b6cb3d",
+    "url": "/static/js/main.cd544acc.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
