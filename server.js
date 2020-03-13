086e8bf3d71cb05126353565643f8b4de30539ee
Mark Langhamer <langhamerm@gmail.com>
2020-03-13 13:16:22 -0500

    yo

--- server.js
+++ server.js
@@ -23,6 +23,20 @@ const usersRouter = require('./routes/users');
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
